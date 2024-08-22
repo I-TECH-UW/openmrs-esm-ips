7b31009e3c9aa4d5ab320d07ca63c7a622f8ac52
cmiiro <christopher.miiro@gmail.com>
2024-08-22 21:48:18 +0300

    Remove redundant page directory

--- src/pages/patient-history-page.ts
+++
@@ -1,9 +0,0 @@
-import { type Page } from '@playwright/test';
-
-export class PatientHistoryPage {
-  constructor(readonly page: Page) {}
-
-  async goTo(uuid: string) {
-    await this.page.goto('/openmrs/spa/patient/' + uuid + '/chart/Patient History');
-  }
-}
